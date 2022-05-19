93b72568a7275df10ac912ec12b4491bad14a95f
Asael Botbol <101400228+AsaelBotbol@users.noreply.github.com>
2022-05-19 15:35:55 -0300

    myCube.SetActive

--- Assets/Activator.cs
+++ Assets/Activator.cs
@@ -9,12 +9,23 @@ public class Activator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        myCube.SetActive(false);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            /*if(myCube.activeInHierarchy == true)
+            {
+                myCube.SetActive(false);
+            }
+            else
+            {
+                myCube.SetActive(true);
+            }*/
+            myCube.SetActive(!myCube.activeInHierarchy);
+        }
     }
 }
