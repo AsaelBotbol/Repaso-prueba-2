4db04813225dae775cc8907f7b320a848b59e9c1
Asael Botbol <101400228+AsaelBotbol@users.noreply.github.com>
2022-05-19 15:19:09 -0300

    SetActive(false)

---
+++ Assets/Activator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activator : MonoBehaviour
+{
+    public GameObject myCube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myCube.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
